0ddd5db8d4e364be6d3fa99a56334a1522c51060
Benji Kay <okaybenji@gmail.com>
2019-08-15 16:50:11 -0500

    script most of the Q&A

--- Locked In/Assets/Scripts/NpcSound.cs
+++ Locked In/Assets/Scripts/NpcSound.cs
@@ -16,7 +16,7 @@ public class NpcSound : MonoBehaviour {
   public AudioClip iCantHearYou;
   public AudioClip knockOnceOrTwice;
   public AudioClip hereComes;
-  public AudioClip huhuhuh;
+  public AudioClip huhuhuh; // lolz did you lose the key or something
   public AudioClip iSaidDidYouLoseIt;
   public AudioClip howIsThatPossible;
   public AudioClip ahhThanks;
@@ -25,9 +25,13 @@ public class NpcSound : MonoBehaviour {
   private float saidICanHearYouAt;
   private float saidPleaseAt;
   private float saidICantHearYouAt;
+  private float saidHuhHuhHuhAt;
   private float explainedSituationAt;
   private float finishedExplanationAt;
   private float lastKnockAt;
+
+  private string currentQuestion = "";
+
   private int knockCount;
   private bool countingKnocks = false;
 
@@ -51,28 +55,46 @@ public class NpcSound : MonoBehaviour {
   }
 
   private IEnumerator sayPlease() {
-
     if (lastKnockAt <= saidICanHearYouAt) {
       audio.PlayOneShot(please);
       saidPleaseAt = Time.time;
 
       yield return new WaitForSeconds(4);
-      sayICantHearYou();
+      StartCoroutine(sayICantHearYou());
     }
   }
 
-  private void sayICantHearYou() {
+  private IEnumerator sayICantHearYou() {
+    currentQuestion = "canYouHearMe";
+
     if (lastKnockAt <= saidPleaseAt) {
       audio.PlayOneShot(iCantHearYou);
       saidICantHearYouAt = Time.time;
+
+      yield return new WaitForSeconds(13);
+      repeatInstructions();
     }
   }
 
-  private IEnumerator explainSituation() {
-    explainedSituationAt = Time.time;
+  private void repeatInstructions() {
+    if (lastKnockAt <= saidICantHearYouAt) {
+      audio.PlayOneShot(knockOnceOrTwice);
+    }
+  }
+
+  private IEnumerator explainSituation(bool veryFunny = false) {
     audio.Stop();
-    audio.PlayOneShot(okayGreatLook);
-    yield return new WaitForSeconds(2);
+
+    explainedSituationAt = Time.time;
+    currentQuestion = "willYouHelp";
+    countingKnocks = false;
+
+    if (veryFunny) {
+      audio.PlayOneShot(veryFunnyLook);
+    } else {
+      audio.PlayOneShot(okayGreatLook);
+    }
+    yield return new WaitForSeconds(3);
     audio.PlayOneShot(iFeelDumb);
     yield return new WaitForSeconds(16);
     audio.PlayOneShot(knockOnceOrTwice);
@@ -80,13 +102,55 @@ public class NpcSound : MonoBehaviour {
     finishedExplanationAt = Time.time;
   }
 
+  private IEnumerator firstKey() {
+    // TODO: ...
+    currentQuestion = "";
+    audio.PlayOneShot(hereComes);
+    yield return new WaitForSeconds(5);
+    audio.PlayOneShot(huhuhuh);
+    saidHuhHuhHuhAt = Time.time;
+    currentQuestion = "didYouLoseIt";
+    yield return new WaitForSeconds(5);
+    sayISaidDidYouLoseIt();
+  }
+
+  private void sayISaidDidYouLoseIt() {
+    if (lastKnockAt < saidHuhHuhHuhAt) {
+      audio.PlayOneShot(iSaidDidYouLoseIt);
+    }
+  }
+
+  private IEnumerator secondKey() {
+    // TODO: ...
+    audio.PlayOneShot(howIsThatPossible);
+    yield return new WaitForSeconds(10);
+    audio.PlayOneShot(ahhThanks);
+  }
+
   private void respondToKnocks(int knockCount) {
     audio.Stop();
 
+    Debug.Log("respondToKnocks: " + knockCount);
+    Debug.Log("current Question: " + currentQuestion);
+
+    // Player knocks once for yes, twice for no.
     if (knockCount == 1) {
-      Debug.Log("player said yes");
+      if (currentQuestion == "canYouHearMe") {
+        StartCoroutine(explainSituation());
+      } else if (currentQuestion == "willYouHelp") {
+        StartCoroutine(firstKey());
+      } else if (currentQuestion == "didYouLoseIt") {
+        StartCoroutine(secondKey());
+      }
     } else if (knockCount == 2) {
-      Debug.Log("player said no");
+      if (currentQuestion == "canYouHearMe") {
+        bool veryFunny = true;
+        StartCoroutine(explainSituation(veryFunny));
+      } else if (currentQuestion == "willYouHelp") {
+        audio.PlayOneShot(reallyComeOn);
+      } else if (currentQuestion == "didYouLoseIt") {
+        StartCoroutine(secondKey());
+      }
     } else if (knockCount >= 15) {
       audio.PlayOneShot(aBunch);
     } else {
@@ -96,13 +160,16 @@ public class NpcSound : MonoBehaviour {
 
   // Communicate to the NPC that the player has just knocked.
   public void knock() {
-    // If we already said hello, the player just knocked, and we haven't yet explained the situation, do that.
-    if ((saidHelloAt + 3) < Time.time && explainedSituationAt == 0) {
-      StartCoroutine(explainSituation());
-    }
-
-    // If we said we can't hear the player talking, start counting their knocks.
-    if (saidICantHearYouAt > 0 && (saidICantHearYouAt + 2) < Time.time) {
+    // We started explaining that we can't hear the player talk...
+    if (saidICantHearYouAt != 0) {
+      // If we said we can't hear the player talking, start counting their knocks.
+      if ((saidICantHearYouAt + 6) < Time.time) {
+        countingKnocks = true;
+      }
+    } else if ((saidHelloAt + 3) < Time.time && explainedSituationAt == 0) {
+        // If we already said hello, the player just knocked, and we haven't yet explained the situation, do that.
+        StartCoroutine(explainSituation());
+    } else if (finishedExplanationAt < Time.time) {
       countingKnocks = true;
     }
 
@@ -116,6 +183,13 @@ public class NpcSound : MonoBehaviour {
   void Update() {
     // If we are counting knocks, wait for a delay and the respond.
     if (countingKnocks && (Time.time - lastKnockAt) > 1) {
+      // We don't want to interrupt the explanation of the situation.
+      if (currentQuestion == "willYouHelp" && finishedExplanationAt == 0) {
+        knockCount = 0;
+        countingKnocks = false;
+        return;
+      }
+
       respondToKnocks(knockCount);
       knockCount = 0;
       countingKnocks = false;

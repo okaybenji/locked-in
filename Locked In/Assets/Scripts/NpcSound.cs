e36ad3465bb494e3ef44b034a9faf23097b8987b
Benji Kay <okaybenji@gmail.com>
2019-08-15 13:39:06 -0500

    block player in once they enter the room

--- Locked In/Assets/Scripts/NpcSound.cs
+++ Locked In/Assets/Scripts/NpcSound.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class NpcSound : MonoBehaviour {
   public AudioClip hello;
+  public AudioClip iCanHearYou;
+  public AudioClip please;
+  public AudioClip okayGreatLook;
+  public AudioClip veryFunnyLook;
+  public AudioClip reallyComeOn;
+  public AudioClip iFeelDumb;
+  public AudioClip iCantHearYou;
+  public AudioClip knockOnceOrTwice;
+  public AudioClip hereComes;
+  public AudioClip huhuhuh;
+  public AudioClip iSaidDidYouLoseIt;
+  public AudioClip howIsThatPossible;
+  public AudioClip ahhThanks;
+
+  private float lastKnock;
 
   public void sayHello() {
-    Debug.Log("here we go!");
     GetComponent<AudioSource>().PlayOneShot(hello, 1.0f);
+  }
+
+  // Communicate to the NPC that the player has just knocked.
+  public void knock() {
+    lastKnock = Time.time;
   }
 }
--- Locked In/Assets/Scripts/PlayerSound.cs
+++ Locked In/Assets/Scripts/PlayerSound.cs
@@ -2,82 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
- public class PlayerSound : MonoBehaviour {
-   // NPC
-   public GameObject npc;
+// Player Sound ended up being too specific. This is just the player control code.
+public class PlayerSound : MonoBehaviour {
+ public GameObject npc;
+ public GameObject invisibleWall;
 
+ // Footsteps
+ public AudioClip stepLeft1;
+ public AudioClip stepLeft2;
+ public AudioClip stepRight1;
+ public AudioClip stepRight2;
+ public float footstepDelay;
+
+ private AudioClip walkSound;
+ private float nextFootstep = 0;
+ private string nextFoot = "left";
+
+ // Door knocking
+ public bool inKnockZone = false;
+ public AudioClip[] knocks;
+ private int knock = 0;
+ private int lastKnock = 0;
+
+ void FixedUpdate () {
    // Footsteps
-   public AudioClip stepLeft1;
-   public AudioClip stepLeft2;
-   public AudioClip stepRight1;
-   public AudioClip stepRight2;
-   public float footstepDelay;
-
-   private AudioClip walkSound;
-   private float nextFootstep = 0;
-   private string nextFoot = "left";
-
-   // Door knocking
-   public bool inKnockZone = false;
-   public AudioClip[] knocks;
-   private int knock = 0;
-   private int lastKnock = 0;
-
-   void FixedUpdate () {
-     // Footsteps
-     if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W)) {
-       nextFootstep -= Time.deltaTime;
-       if (nextFootstep <= 0) {
-         if (nextFoot == "left") {
-           if (Random.Range(0.0f, 1.0f) > 0.5) {
-             Debug.Log("left1");
-             walkSound = stepLeft1;
-           } else {
-             Debug.Log("left2");
-             walkSound = stepLeft2;
-           }
-           nextFoot = "right";
+   if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W)) {
+     nextFootstep -= Time.deltaTime;
+     if (nextFootstep <= 0) {
+       if (nextFoot == "left") {
+         if (Random.Range(0.0f, 1.0f) > 0.5) {
+           walkSound = stepLeft1;
          } else {
-            if (Random.Range(0.0f, 1.0f) > 0.5) {
-             walkSound = stepRight1;
-              Debug.Log("right1");
-           } else {
-             walkSound = stepRight2;
-              Debug.Log("right2");
-           }
-           nextFoot = "left";
+           walkSound = stepLeft2;
          }
-         GetComponent<AudioSource>().PlayOneShot(walkSound, 1.0f);
-         nextFootstep += footstepDelay;
+         nextFoot = "right";
+       } else {
+          if (Random.Range(0.0f, 1.0f) > 0.5) {
+           walkSound = stepRight1;
+         } else {
+           walkSound = stepRight2;
+         }
+         nextFoot = "left";
        }
-     }
-   }
-
-   void Update() {
-     // Door knocking
-     if (Input.GetMouseButtonDown(0) && inKnockZone) {
-       // Make sure we don't play the same sound twice in a row.
-       while (knock == lastKnock) {
-         knock = Random.Range(0, 4);
-       }
-       GetComponent<AudioSource>().PlayOneShot(knocks[knock], 1.0f);
-       lastKnock = knock;
-     }
-   }
-
-   void OnTriggerEnter(Collider other) {
-     if (other.tag == "start") {
-       npc.GetComponent<NpcSound>().sayHello();
-     }
-
-     if (other.tag == "knockZone") {
-       inKnockZone = true;
-     }
-   }
-
-   void OnTriggerExit(Collider other) {
-     if (other.tag == "knockZone") {
-       inKnockZone = false;
+       GetComponent<AudioSource>().PlayOneShot(walkSound, 1.0f);
+       nextFootstep += footstepDelay;
      }
    }
  }
+
+ void Update() {
+   // Door knocking
+   if (Input.GetMouseButtonDown(0) && inKnockZone) {
+     // Make sure we don't play the same sound twice in a row.
+     while (knock == lastKnock) {
+       knock = Random.Range(0, 4);
+     }
+     GetComponent<AudioSource>().PlayOneShot(knocks[knock], 1.0f);
+     npc.GetComponent<NpcSound>().knock();
+     lastKnock = knock;
+   }
+ }
+
+ void OnTriggerEnter(Collider other) {
+   if (other.tag == "start") {
+     npc.GetComponent<NpcSound>().sayHello();
+     // Block the player in so they don't wander off.
+     invisibleWall.GetComponent<MeshCollider>().enabled = true;
+   }
+
+   if (other.tag == "knockZone") {
+     inKnockZone = true;
+   }
+ }
+
+ void OnTriggerExit(Collider other) {
+   if (other.tag == "knockZone") {
+     inKnockZone = false;
+   }
+ }
+}

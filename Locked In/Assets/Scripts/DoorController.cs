b03513e7d00fc5fdd6ce987c450627c3b99115df
Benji Kay <okaybenji@gmail.com>
2019-09-12 16:24:15 -0500

    highlight door when player has key

--- Locked In/Assets/Scripts/DoorController.cs
+++ Locked In/Assets/Scripts/DoorController.cs
@@ -9,7 +9,15 @@ public class DoorController : MonoBehaviour {
   public float openSpeed = 10.0f;
   public AudioClip openDoor;
 
+  // Outline effect.
+  public Camera camera;
+  public GameObject player;
+
   private float openStart;
+
+  void Start() {
+    GetComponentInChildren<Outline>().enabled = false;
+  }
 
   public void open() {
     isOpen = true;
@@ -25,6 +33,18 @@ public class DoorController : MonoBehaviour {
         GetComponentInChildren<MeshRenderer>().enabled = false;
         Destroy(GetComponentInChildren<MeshCollider>());
         Destroy(this);
+      }
+    } else if (player.GetComponent<PlayerController>().hasKey) {
+      // Show an outline when player looks at door if they have the key.
+      // Determine if player is looking at me.
+      RaycastHit hit;
+      Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+      // If so, add a glow effect. If not, remove it.
+      if (Physics.Raycast(ray, out hit)) {
+        GetComponentInChildren<Outline>().enabled = true;
+       } else {
+        GetComponentInChildren<Outline>().enabled = false;
       }
     }
   }
